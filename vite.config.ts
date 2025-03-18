52455364994e3776379303e0f61e3d86ae7fd8d6
21120447 <haonhat2729@gmail.com>
2025-03-18 14:35:57 +0700

    feat: add source code

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/visai",
   plugins: [react(), tailwindcss()],
   server: {
     port: 5173,
